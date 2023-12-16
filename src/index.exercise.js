414dcdefe88af7851a5570de9c2a0919e53791e5
patri <patrickmarkgraf@gmail.com>
2023-12-16 09:33:05 +0100

    ex1-extra1: add @reach dialog for login and register

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,14 +1,36 @@
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {Logo} from './components/logo'
+import '@reach/dialog/styles.css'
+import {Dialog} from '@reach/dialog'
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none')
+
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      <button onClick={() => alert('Login clicked')}>Login</button>
-      <button onClick={() => alert('Register clicked')}>Register</button>
+      <button onClick={() => setOpenModal('login')}>Login</button>
+      <button onClick={() => setOpenModal('register')}>Register</button>
+      <Dialog
+        isOpen={openModal === 'login'}
+        onDismiss={() => setOpenModal('none')}
+      >
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h2>Login</h2>
+      </Dialog>
+      <Dialog
+        isOpen={openModal === 'register'}
+        onDismiss={() => setOpenModal('none')}
+      >
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h2>Register</h2>
+      </Dialog>
     </div>
   )
 }
